5ecbf68d3477d32b11cf0e1c9aa6a787f5262eb4
Marcelo Lazo <marcelolazo19972013@hotmail.com>
2019-08-15 05:26:33 -0400

    Menu Done

---
+++ Assets/Scripts/MenuPlayer.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class MenuPlayer : MonoBehaviour
+{
+    [Header("Main Icons")]
+    [SerializeField] private GameObject selectorGO;
+    [SerializeField] private GameObject megamanIcon;
+    [Header("Grid Position")]
+    [SerializeField] private int actualPos = 4;
+    [Header("Miscellaneous")]
+    [SerializeField] private List<Transform> listTransformGrid = new List<Transform>();
+    [SerializeField] private List<Sprite> listSpritePlayer = new List<Sprite>();
+    private bool enemySelected = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.VerifyResources();
+    }
+
+    private void VerifyResources(){
+        if(this.listSpritePlayer.Count == 0){
+            Sprite[] spriteEx = Resources.LoadAll <Sprite> ("MegamanFace") as Sprite[];  
+            foreach (Sprite t in spriteEx)
+            {
+                this.listSpritePlayer.Add(t);
+            }
+        }
+        if(this.listTransformGrid.Count == 0){
+            Transform ts = this.transform.Find("SelectorPosition");
+            for (int i = 0; i < ts.childCount; i++){
+                listTransformGrid.Add(ts.GetChild(i));
+            }
+        }
+        if(this.selectorGO == null){
+            this.selectorGO = this.transform.Find("Selector").gameObject;
+        }
+        if(this.megamanIcon == null){
+            this.megamanIcon = this.transform.Find("Player").gameObject;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // float vMov = Input.GetAxisRaw("Vertical"); //-1 aba || +1 arr
+        // float hMov = Input.GetAxisRaw("Horizontal");//-1 izq || +1 der
+        if(Input.GetKeyDown(KeyCode.Return) && this.actualPos != 4){
+            this.enemySelected = true;
+            this.transform.Find("Selector").GetComponent<Animator>().SetTrigger("EnemySelected");
+        }
+        
+        if(this.enemySelected == false){
+            //VERTICAL INPUT
+            float UPMOV = Input.GetKeyDown(KeyCode.UpArrow) ? 1f:0f;
+            float DOWNMOV = Input.GetKeyDown(KeyCode.DownArrow) ? -1f:0f;
+            float vMov = UPMOV + DOWNMOV;
+            //HORIZONTAL INPUT
+            float RIGHTMOV = Input.GetKeyDown(KeyCode.RightArrow) ? 1f:0f;
+            float LEFTMOV = Input.GetKeyDown(KeyCode.LeftArrow) ? -1f:0f;
+            float hMov = RIGHTMOV + LEFTMOV;
+            //CASOS PARA VMOV
+            if(vMov == 1){// ARRIBA
+                if(this.actualPos > 2){
+                    this.actualPos -= 3;
+                }
+            }else if(vMov == -1){ // ABAJO
+                if(this.actualPos < 6){
+                    this.actualPos += 3;
+                }
+            }
+
+            //CASOS PARA HMOV
+            if(hMov == 1){ //DERECHA
+                if(this.actualPos != 2 && this.actualPos != 5 && this.actualPos != 8){
+                    this.actualPos +=1;
+                }
+            }else if(hMov == -1 ){ //IZQUIERDA
+                if(this.actualPos != 0 && this.actualPos != 3 && this.actualPos != 6){
+                    this.actualPos -=1;
+                }
+            }
+            UpdateSelectorPosition();
+            UpdateMegamanIcon();
+        }
+    }
+
+    private void UpdateSelectorPosition(){
+        this.selectorGO.transform.localPosition = this.listTransformGrid[this.actualPos].localPosition;
+    }
+
+    private void UpdateMegamanIcon(){
+        this.megamanIcon.transform.GetComponent<Image>().sprite = this.listSpritePlayer[this.actualPos];
+    }
+}
---
+++ Assets/Scripts/MenuSetter.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+public class MenuSetter : MonoBehaviour
+{
+    [Header("Enemy Prefab")]
+    public GameObject enemyIconPrefab;
+    [Header("Miscellaneous")]
+    [SerializeField] private List<Transform> listTransformGrid = new List<Transform>();
+    [SerializeField] private List<Sprite> listSpriteEnemy = new List<Sprite>();
+    [SerializeField] private List<Sprite> listSpritePlayer = new List<Sprite>();
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.VerifyResources();
+        this.SetUpEnemies();
+    }
+
+    private void SetUpEnemies(){
+        for(int i=0;i<this.listSpriteEnemy.Count;i++){
+            GameObject enemyGO = (GameObject)Instantiate(this.enemyIconPrefab,this.transform.position,Quaternion.identity);
+            enemyGO.transform.SetParent(this.transform);
+            enemyGO.transform.localScale = new Vector3(1f,1f,1f);
+            enemyGO.transform.localPosition = this.listTransformGrid[i + i/4].localPosition;
+            enemyGO.transform.GetComponent<Image>().sprite = this.listSpriteEnemy[i];
+        }
+    }
+
+    private void VerifyResources(){
+        if(this.enemyIconPrefab == null){
+            this.enemyIconPrefab = Resources.Load("Prefab/EnemyIcon", typeof(GameObject)) as GameObject;
+        }
+        if(this.listSpritePlayer.Count == 0){
+            Sprite[] spriteEx = Resources.LoadAll <Sprite> ("MegamanFace") as Sprite[];  
+            foreach (Sprite t in spriteEx)
+            {
+                this.listSpritePlayer.Add(t);
+            }
+        }
+        if(this.listSpriteEnemy.Count == 0){
+            Sprite[] spriteEx = Resources.LoadAll <Sprite> ("EnemiesFace") as Sprite[];  
+            for(int i=0;i<spriteEx.Length;i++){
+                if(i!=4){
+                    this.listSpriteEnemy.Add(spriteEx[i]);
+                }
+            }
+        }
+        if(this.listTransformGrid.Count == 0){
+            Transform ts = this.transform.Find("SelectorPosition");
+            for (int i = 0; i < ts.childCount; i++){
+                listTransformGrid.Add(ts.GetChild(i));
+            }
+        }
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
